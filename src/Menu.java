00c04100ec7f662d28c1642e76b427bb49bbc694
Tyson Olychuck <Olychuck@gmail.com>
2012-11-08 23:42:07 -0800

    Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
 public class Menu {
-
+	public ArrayList<Item> arr;
+	public Menu(){
+		arr = new ArrayList<Item>();
+	}
+	public void prefill(){
+		arr.add(new Item(17.99,"Chicken Parmesan"));
+		arr.add(new Item(15.99,"Fettucini Alfredo"));
+		arr.add(new Item(10.99,"Ceasar Salad"));
+		arr.add(new Item(12.99,"Hamburger"));
+		arr.add(new Item(14.99,"Personal Pizza"));
+		arr.add(new Item(17.99,"Cheesecake"));
+	}
 }
