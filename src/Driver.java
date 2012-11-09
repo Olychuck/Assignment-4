20ff6dafbe3ce6328bd924be131bb9978257099a
Tyson Olychuck <Olychuck@gmail.com>
2012-11-08 23:58:50 -0800

    part2

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 
 public class Driver {
 
@@ -5,7 +7,12 @@ public class Driver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkedList<Waiter> qu = new LinkedList<Waiter>();
+		qu.add(new Waiter("Steve"));
+		qu.add(new Waiter("George"));
+		
+		
+		
 
 	}
 
--- src/Waiter.java
+++ src/Waiter.java
@@ -3,8 +3,9 @@ import java.util.ArrayList;
 public class Waiter {
 	double tips;
 	ArrayList<Bill> bills;
-
-	public Waiter() {
+	String name;
+	public Waiter(String t) {
+		name = t;
 		tips = 0;
 		bills = new ArrayList<Bill>();
 	}
