8ebd320ee3483f0bd61ac1260cec6721ce164a08
Tyson Olychuck <Olychuck@gmail.com>
2012-11-08 23:35:31 -0800

    waiter,bill,menu,item

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,12 @@
 
 public class Item {
-
+	double price;
+	String name;
+	public Item(double price,String name){
+		this.price=price;
+		this.name=name;
+	}
+	public String toString(){
+		return name +": "+price;
+	}
 }
--- src/Waiter.java
+++ src/Waiter.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
 
 public class Waiter {
+	double tips;
+	ArrayList<Bill> bills;
 
+	public Waiter() {
+		tips = 0;
+		bills = new ArrayList<Bill>();
+	}
 }
